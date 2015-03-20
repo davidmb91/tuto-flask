9bf1f0cc582d635085325fae497cc72a8fa7912a
David MB <davidmb91@gmail.com>
2015-03-20 17:54:03 +0100

    Parte 2 - templates, condiciones y bucles

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
- 
+
 @app.route('/')
 def hello_world():
-    return 'Hola amigos/as!'
- 
+    titulo = "tutoFlask"
+    usuario = [{'nombre': 'Alejandro'},
+				{'nombre': 'Miguel'},
+				{'nombre': 'Mario'},
+				{'nombre': 'Osmary'},
+				{'nombre': 'Dachi'},
+				{'nombre': 'Luis'},
+				{'nombre': 'Aldo'}]
+    return render_template('index.html',
+                           titulo=titulo,
+                           usuario=usuario)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
